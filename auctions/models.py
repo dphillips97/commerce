1cdd31c38b94ae89125974b837b56ad81ee53387
Drew <dphillips97@gmail.com>
2022-07-20 13:45:04 -0600

    Listing model and form working

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,46 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.forms import ModelForm
+from django.core.exceptions import ValidationError
+from django.conf import settings
 
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+
+    class Category(models.TextChoices):
+        # Define choices
+        CLOTHING = "C", "Clothing"
+        ELECTRONICS = "E", "Electronics"
+        EVERYTHING = "EE", "Everything Else"
+
+    # Auto-generated
+    item_id = models.AutoField(primary_key=True)
+    active = models.BooleanField(default=True)
+    lister_id = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                    on_delete=models.CASCADE,
+                                    null=True,
+                                    blank=True)
+
+    # User-generated
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    image_url = models.URLField(max_length=512, blank=True)
+    category = models.CharField(max_length=2,
+                                choices=Category.choices,
+                                blank=True,
+                                default=None)
+    initial_bid = models.DecimalField(max_digits=10,
+                                        decimal_places=2,
+                                        default=1)
+
+    def __str__(self):
+        return f"{self.item_id}: {self.title} listed by {self.lister_id}. Active={self.active}"
+
+# Create form from model to pass to create_listing page
+class ListingForm(ModelForm):
+    class Meta:
+        model = Listing
+        fields=['title', 'description', 'image_url', 'category', 'initial_bid']
--- auctions/views.py
+++ auctions/views.py
@@ -3,12 +3,18 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from auctions.models import Listing, ListingForm
 
 from .models import User
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+
+    active_listings = Listing.objects.filter(active=True)
+
+    return render(request, "auctions/index.html", {
+        "active_listings": active_listings
+        })
 
 
 def login_view(request):
@@ -61,3 +67,29 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def create(request):
+    if request.method == "GET":
+        
+        listing_form = ListingForm()
+
+        return render(request, "auctions/create_listing.html", {
+            "form": listing_form
+            })
+
+    if request.method == "POST":
+
+        listing_form_post = ListingForm(request.POST)
+
+        if listing_form_post.is_valid():
+
+            listing_form_post.save()
+
+            return HttpResponseRedirect("/")
+
+        else:
+
+            return render(request, "auctions/create_listing.html", {
+                "form": listing_form_post
+                })
+
